d7665ba843528e1348ed2530b43042f1038bb888
Kyle Li <Kyle.Li@lombardrisk.com>
2016-07-26 19:32:43 +0800

    week4

--- scala-course/src/main/scala/course1/week4/Boolean.scala
+++ scala-course/src/main/scala/course1/week4/Boolean.scala
@@ -1,21 +1,21 @@
 package course1.week4
 
-abstract class Boolean2 {
+abstract class Boolean {
   def ifThenElse[T](t: => T, e: => T): T
   
-  def && (x: => Boolean2): Boolean2 = ifThenElse(x, false2)
-  def || (x: => Boolean2): Boolean2 = ifThenElse(true2, x)
-  def unary_!: Boolean2 = ifThenElse(false2, true2)
-  
-  def == (x: Boolean2): Boolean2 = ifThenElse(x, x.unary_!)
-  def != (x: Boolean2): Boolean2 = ifThenElse(x/unary_!, x)
-  def < (x: Boolean2): Boolean2 = ifThenElse(x, x.unary_!)
+  def && (x: => Boolean): Boolean = ifThenElse(x, false2)
+  def || (x: => Boolean): Boolean = ifThenElse(true2, x)
+  def unary_! : Boolean = ifThenElse(false2, true2)
+
+  def == (x: Boolean): Boolean = ifThenElse(x, x.unary_!)
+  def != (x: Boolean): Boolean = ifThenElse(x.unary_!, x)
+  def < (x: Boolean): Boolean = ifThenElse(false2, x)
 }
 
-object true2 extends Boolean2 {
+object true2 extends Boolean {
   def ifThenElse[T](t: => T, e: => T): T = t
 }
 
-object false2 extends Boolean2 {
+object false2 extends Boolean {
   def ifThenElse[T](t: => T, e: => T): T = e
 }
