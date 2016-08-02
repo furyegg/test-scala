a87add3f13751bf50ae1fb01a42b556f1fad476c
lyhtbc <lyhtbc@gmail.com>
2016-08-02 08:58:10 +0800

    week5

--- scala-course/src/main/scala/course1/week5/ListImpl.scala
+++ scala-course/src/main/scala/course1/week5/ListImpl.scala
@@ -7,11 +7,34 @@ object ListImpl {
     case y :: ys => y :: init(ys)
   }
   
-  def removeAt[T](n: Int, xs: List[T]) = ???
+  def removeAt[T](n: Int, xs: List[T]): List[T] = {
+    def remove(xs: List[T], index: Int): List[T] = xs match {
+      case Nil => Nil
+      case y :: ys => if (index == n) remove(ys, index + 1) else y :: remove(ys, index + 1)
+    }
+    remove(xs, 0)
+  }
+  
+  def flatten(xs: List[Any]): List[Any] = {
+    def doFlatten(xs: Any, result: List[Any]): List[Any] = xs match {
+      case Nil =>
+        Nil
+      case y :: ys =>
+        result ++ doFlatten(y, result) ++ doFlatten(ys,  result)
+      case x =>
+        result :+ x
+    }
+    doFlatten(xs, Nil)
+  }
   
   def main(args: Array[String]): Unit = {
     val list = List(1,2,3,4)
     println(init(list))
     println(removeAt(1, list))
+  
+    val f = flatten(List(List(1,1), 2, List(3, List(5,8))))
+    println(f)
+    
+    // f.flatten
   }
 }
