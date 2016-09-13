1d281fd7dc7b5c83a980a49173f882e61d11f387
lyhtbc <lyhtbc@gmail.com>
2016-09-13 22:10:41 +0800

    week11

---
+++ scala-course/src/main/scala/course3/week1/ParallelismJVM1.sc
@@ -0,0 +1,24 @@
+var uidCount = 0L
+
+def getUniqueId(): Long = {
+  uidCount = uidCount + 1
+  uidCount
+}
+
+def startThread() = {
+  val t = new Thread {
+    override def run(): Unit = {
+      val uids = for (i <- 0 until 10) yield getUniqueId()
+      println(uids)
+    }
+  }
+  // t.start()
+  t
+}
+
+val t1 = startThread()
+val t2 = startThread()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
---
+++ scala-course/src/main/scala/course3/week1/ParallelismJVM1.scala
@@ -0,0 +1,27 @@
+package course3.week1
+
+object ParallelismJVM1 {
+  private var uidCount = 0L
+  private val x = new AnyRef()
+  
+  def getUniqueId(): Long = x.synchronized {
+    uidCount = uidCount + 1
+    uidCount
+  }
+  
+  def startThread() = {
+    val t = new Thread {
+      override def run(): Unit = {
+        val uids = for (i <- 0 until 10) yield getUniqueId()
+        println(uids)
+      }
+    }
+    t.start()
+    t
+  }
+  
+  def main(args: Array[String]): Unit = {
+    startThread()
+    startThread()
+  }
+}
