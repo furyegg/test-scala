b9ae03ed68e69b623765b810104df1600799ae1c
lyhtbc <lyhtbc@gmail.com>
2017-09-17 12:46:43 +0800

    finished chapter 4

--- fpinscala/exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ fpinscala/exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,7 +1,9 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import java.util.regex.{Pattern, PatternSyntaxException}
+
+import scala.{Either => _, Option => _, Some => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
@@ -27,7 +29,7 @@ sealed trait Option[+A] {
 
   def orElse1[B>:A](ob: => Option[B]): Option[B] = this match {
     case None => ob
-    case _ => _
+    case _ => this
   }
 
   def filter(f: A => Boolean): Option[A] = this match {
@@ -40,6 +42,16 @@ case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  
+  def main(args: Array[String]): Unit = {
+    val l1 = List(Some(1), Some(2), Some(3))
+    val l2 = List(None)
+    val l3 = List(None, Some(1))
+    println(sequence(l1))
+    println(sequence(l2))
+    println(sequence(l3))
+  }
+  
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try {
@@ -61,11 +73,38 @@ object Option {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
   
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+  
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (aa => b map (bb => f(aa, bb)))
+  
+  def pattern(s: String): Option[Pattern] =
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case e: PatternSyntaxException => None
+    }
+  
+  def mkMatcher(pat: String): Option[String => Boolean] =
+    pattern(pat) map (p => (s: String) => p.matcher(s).matches)
+  
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] =
+    map2(mkMatcher(pat1), mkMatcher(pat2))(
+      (f1, f2) => f1(s) && f2(s)
+    )
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case Some(x) :: xs => sequence(xs) flatMap (xs => Some(x :: xs))
+    case _ => None
+  }
+  
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
-
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldLeft[Option[List[B]]](Some(Nil))((z, x) => map2(z, f(x))(_ :+ _))
 }
