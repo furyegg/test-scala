4b1f3368b3f2aa40a8a17907ba87f37050d09042
lyhtbc <lyhtbc@gmail.com>
2017-10-15 21:28:47 +0800

    chapter 11

--- fpinscala/exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ fpinscala/exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -36,24 +36,45 @@ trait Monad[M[_]] extends Functor[M] {
   def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] =
+    lma.foldLeft(unit(List[A]()))((ml, ma) => map2(ma, ml)(_ :: _))
 
-  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] =
+    la.foldLeft(unit(List[B]()))((ml, a) => map2(f(a), ml)(_ :: _))
 
-  def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
+  def replicateM[A](n: Int, ma: M[A]): M[List[A]] =
+    sequence(List.fill(n)(ma))
+  
+  def product[A,B](ma: M[A], mb: M[B]): M[(A, B)] = map2(ma, mb)((_, _))
+  
+  def filterM[A](ms: List[A])(f: A => M[Boolean]): M[List[A]] =
+//    map {
+//      traverse(ms)(a => map(f(a)) { b =>
+//        if (b) List(a) else List()
+//      })
+//    } (_.flatten)
+    ms match {
+      case Nil => unit(Nil)
+      case h :: t => flatMap(f(h))(b =>
+        if (!b) filterM(t)(f)
+        else map(filterM(t)(f))(h :: _))
+    }
 
-  def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] = ???
+  def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] =
+    a => flatMap(f(a))(g)
 
   // Implement in terms of `compose`:
-  def _flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def _flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] =
+    compose((_: Unit) => ma, f)(())
 
-  def join[A](mma: M[M[A]]): M[A] = ???
+  def join[A](mma: M[M[A]]): M[A] =
+    flatMap(mma)(ma => ma)
 
   // Implement in terms of `join`:
-  def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] =
+    // join(unit(f(ma))) => wrong
+    join(map(ma)(f))
 }
-
-case class Reader[R, A](run: R => A)
 
 object Monad {
   val genMonad = new Monad[Gen] {
@@ -62,32 +83,61 @@ object Monad {
       ma flatMap f
   }
 
-  val parMonad: Monad[Par] = ???
+  lazy val parMonad: Monad[Par] = ???
 
   def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = ???
+  
+  lazy val optionMonad: Monad[Option] = ???
+  
+  lazy val streamMonad: Monad[Stream] = ???
 
-  val optionMonad: Monad[Option] = ???
+  val listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](ma: List[A])(f: (A) => List[B]): List[B] = ma.flatMap(f)
+    override def unit[A](a: => A): List[A] = List(a)
+  }
+  
+  // exercise 2
+  class StateMonads[S] {
+    type StateS[A] = State[S, A]
+    
+    // We can then declare the monad for the `StateS` type constructor:
+    val monad = new Monad[StateS] {
+      def unit[A](a: => A): State[S, A] = State(s => (a, s))
+      def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+        st flatMap f
+    }
+  }
+  
+  def stateMonad[S] = new Monad[({type f[x] = State[S, x]})#f] {
+    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
 
-  val streamMonad: Monad[Stream] = ???
-
-  val listMonad: Monad[List] = ???
-
-  def stateMonad[S] = ???
-
-  val idMonad: Monad[Id] = ???
-
-  def readerMonad[R] = ???
-}
-
-case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B] = ???
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
-}
-
-object Reader {
-  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
-    def unit[A](a: => A): Reader[R,A] = ???
-    override def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = ???
+  val idMonad: Monad[Id] = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = Id(a)
+    override def flatMap[A, B](ma: Id[A])(f: (A) => Id[B]): Id[B] = ma.flatMap(f)
+  }
+  
+  def main(args: Array[String]): Unit = {
+    val id = Id("hello, ").flatMap(a =>
+      Id("Monad!").flatMap(b =>
+        Id(a + b)))
+    println(id)
   }
 }
 
+case class Id[A](value: A) {
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+}
+
+case class Reader[R, A](run: R => A)
+
+object Reader {
+  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
+    def unit[A](a: => A): Reader[R,A] = Reader(_ => a)
+    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+      Reader(r => f(st.run(r)).run(r))
+  }
+}
--- scala-course/src/main/scala/test.sc
+++ scala-course/src/main/scala/test.sc
@@ -1,4 +1,1 @@
-val arr1 = Array(1)
-val (l, r) = arr1.splitAt(arr1.length / 2)
-l.foldLeft("")((s, n) => s+","+n)
-r.foldLeft("")((s, n) => s+","+n)
+type IN = Int
